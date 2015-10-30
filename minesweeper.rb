5a3b8bb2ebfa6a917a29c3785bdca1dfbc28942a
App Academy Student <student@appacademy.io>
2015-10-30 11:42:16 -0700

    initial

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,1 +1,15 @@
-puts 'hello'
+class Tile
+
+  attr_accessor :bombed?, :flagged?, :revealed?
+
+  def initialize(board)
+      @board = board
+      @bombed? = bombed?
+      @flagged? = flagged?
+      @revealed? = revealed?
+  end
+
+
+
+
+end
