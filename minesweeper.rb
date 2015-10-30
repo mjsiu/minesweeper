4bbd612a307c267b47f80fd7a6be4ef8cda83a51
App Academy Student <student@appacademy.io>
2015-10-30 12:05:44 -0700

    Added some psuedo code

--- minesweeper.rb
+++ minesweeper.rb
@@ -6,8 +6,26 @@ class Tile
       @board = board
       @bombed = bombed
       @flagged = flagged
-      @revealed = revealed
+      @revealed = false
   end
+
+  def reveal
+    @revealed = true
+  end
+
+  POSSIBLE_MOVES = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
+
+
+  def neighbors(pos)
+    possible_neighbors = []
+    # it needs to find all of this tile's neighbors
+
+
+
+
+
+  end
+
 
 
 
